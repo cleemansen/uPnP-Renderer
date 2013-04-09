1a676eaf0975c157d6c5017eec6025b498ca517e
Clemens <cleemansen@gmail.com>
2013-04-09 16:01:03 +0200

    # faster interval of event fire (better pos in client)

--- uPnP-Renderer/src/main/java/org/unividuell/upnp/renderer/EventFire.java
+++ uPnP-Renderer/src/main/java/org/unividuell/upnp/renderer/EventFire.java
@@ -1,25 +1,25 @@
 package org.unividuell.upnp.renderer;
 
-import org.fourthline.cling.support.lastchange.LastChangeAwareServiceManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.fourthline.cling.support.lastchange.*;
+import org.slf4j.*;
 
 public class EventFire implements Runnable {
     
     final Logger logger = LoggerFactory.getLogger(EventFire.class);
     
-    private LastChangeAwareServiceManager manager;
+    private final LastChangeAwareServiceManager manager;
     
     public EventFire(LastChangeAwareServiceManager manager) {
         this.manager = manager;
     }
 
+    @Override
     public void run() {
         while (true) {
             logger.debug("FIRE!");
             manager.fireLastChange();
             try {
-                Thread.sleep(1000);
+                Thread.sleep(300);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
